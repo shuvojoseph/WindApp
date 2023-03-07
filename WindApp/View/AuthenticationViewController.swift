44f06363fe8982baaf04136c11ecc28f9839361d
Shuvo Joseph <shuvojoseph@gmail.com>
2023-03-07 19:04:37 +0600

    # Design the basics of firstView (AuthenticationViewController), using autoLayout, SF Symbols. https://stackoverflow.com/a/29209623/1230449

--- WindApp/View/AuthenticationViewController.swift
+++ WindApp/View/AuthenticationViewController.swift
@@ -12,6 +12,17 @@ class AuthenticationViewController: UIViewController {
 
     @IBOutlet var pinView: SVPinView!
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configurePinView()
