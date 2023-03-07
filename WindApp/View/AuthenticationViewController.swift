1b5a8b0fa0f84c83d89aad22808bb2cbe9919d17
Shuvo Joseph <shuvojoseph@gmail.com>
2023-03-07 17:50:46 +0600

    #Added 3rd party library SVPinView using pod for accepting pin numbers. https://github.com/xornorik/SVPinView

--- WindApp/View/AuthenticationViewController.swift
+++ WindApp/View/AuthenticationViewController.swift
@@ -6,13 +6,76 @@
 //
 
 import UIKit
+import SVPinView
 
 class AuthenticationViewController: UIViewController {
 
+    @IBOutlet var pinView: SVPinView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configurePinView()
         // Do any additional setup after loading the view.
     }
+    
+    func configurePinView() {
+        
+        pinView.pinLength = 4
+        pinView.secureCharacter = "\u{25CF}"
+        pinView.interSpace = 10
+        pinView.textColor = UIColor.black
+        pinView.borderLineColor = UIColor.systemGray
+        pinView.activeBorderLineColor = UIColor.black
+        pinView.borderLineThickness = 1
+        pinView.shouldSecureText = true
+        pinView.allowsWhitespaces = false
+        pinView.style = .underline
+        
+        pinView.placeholder = "****"
+        pinView.deleteButtonAction = .deleteCurrentAndMoveToPrevious
+        pinView.keyboardAppearance = .default
+        pinView.tintColor = .white
+        pinView.becomeFirstResponderAtIndex = 0
+        pinView.shouldDismissKeyboardOnEmptyFirstField = false
+        
+        pinView.font = UIFont.systemFont(ofSize: 15)
+        pinView.keyboardType = .phonePad
+        pinView.pinInputAccessoryView = { () -> UIView in
+            let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+            doneToolbar.barStyle = UIBarStyle.default
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+            let done: UIBarButtonItem  = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector(dismissKeyboard))
+            
+            var items = [UIBarButtonItem]()
+            items.append(flexSpace)
+            items.append(done)
+            
+            doneToolbar.items = items
+            doneToolbar.sizeToFit()
+            return doneToolbar
+        }()
+        
+        pinView.didFinishCallback = didFinishEnteringPin(pin:)
+        pinView.didChangeCallback = { pin in
+            print("The entered pin is \(pin)")
+        }
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+    
+    func didFinishEnteringPin(pin:String) {
+        showAlert(title: "Success", message: "The Pin entered is \(pin)")
+    }
+    
+    // MARK: Helper Functions
+    func showAlert(title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func gotoSendFund(_ sender: Any) {
         
         let sendFundViewController = self.storyboard?.instantiateViewController(withIdentifier: "sendFund") as! SendFundViewController
